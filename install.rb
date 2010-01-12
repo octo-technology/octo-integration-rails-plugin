6fa1f693ca4340753039750dc089b880351c26af
Ivan Schneider - ivan <isc@massivebraingames.com>
2010-01-12 17:58:11 +0100

    creating migrate folder if necessary

--- install.rb
+++ install.rb
@@ -1,5 +1,7 @@
 require 'fileutils'
 migrations = Dir.glob(File.join(File.dirname(__FILE__), 'migrations', '*.rb'))
-dest = File.join(File.dirname(__FILE__), '..', '..', '..', 'db', 'migrate', '')
+migrate_folder = File.join(File.dirname(__FILE__), '..', '..', '..', 'db', 'migrate')
+dest = File.join(migrate_folder, '')
+FileUtils.mkdir migrate_folder, :verbose => true unless File.exists? migrate_folder
 FileUtils.cp migrations, dest, :verbose => true
 puts "run script/plugin install git://github.com/rails/open_id_authentication.git"
