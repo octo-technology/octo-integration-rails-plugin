b7e9caca77e01e353186adc1b88175a5c8e135cc
Jessy Bernal <jessy@jessy-bernals-imac-3.local>
2010-01-13 23:28:34 +0100

    plugin install

--- install.rb
+++ install.rb
@@ -2,16 +2,14 @@ require 'fileutils'
 current_path = File.dirname(__FILE__)
 
 migrations = Dir.glob(File.join(current_path, 'migrations', '*.rb'))
-images = Dir.glob(File.join(current_path, 'public', 'images', '*'))
 migrate_folder = File.join(RAILS_ROOT, 'db', 'migrate')
-images_folder = File.join(RAILS_ROOT, 'public', 'images', 'octo-rails')
-
 FileUtils.mkdir migrate_folder, :verbose => true unless File.exists? migrate_folder
 FileUtils.cp migrations, File.join(migrate_folder, ''), :verbose => true
 
-FilteUtils.mkdir images_folder, :verbose => true unless File.exists? images_folder
-FilteUtils.cp images, File.join(images_folder, ''), :verbose => true
-
+images = Dir.glob(File.join(current_path, 'public', 'images', 'octo-rails', '*'))
+images_folder = File.join(RAILS_ROOT, 'public', 'images', 'octo-rails')
+FileUtils.mkdir images_folder, :verbose => true unless File.exists? images_folder
+FileUtils.cp images, File.join(images_folder, ''), :verbose => true
 
 infos = <<EOF
 
